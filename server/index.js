ae7c1d2f81b456d65ce13fb199eaa2e1c13731de
Youzhu <shiyouzhu89@gmail.com>
2020-04-18 10:19:36 -0700

    replace  local repos with ec2 intances

--- server/index.js
+++ server/index.js
@@ -4,18 +4,10 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/../public/'));
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
-});
-
-app.get('/', function (req, res) {
-  if (err) {
-    res.sendStatus(500);
-  } else {
-    res.sendStatus(200);
-  }
 });
 
 
